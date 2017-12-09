2b5b8c1057ca3500a45365235f440450db4fd121
Martin Fredriksson <martin.fredriksson@martinfredriksson.com>
2017-12-09 11:58:01 +0100

    Day 9 part 2

--- 9/day9.py
+++ 9/day9.py
@@ -7,25 +7,30 @@ def main():
 	groupLevel = 0
 	inGarbage = False
 	score = 0
+	garbageCount = 0
 	while(char < len(inputdata)-1):
+		c = inputdata[char] 
 
-		if(inputdata[char] == "<"):
+		if(inGarbage and c != ">" and c != "!"):
+			garbageCount += 1
+
+		if(c == "<"):
 			inGarbage = True
-		elif(inputdata[char] == ">"):
+		elif(c == ">"):
 			inGarbage = False
 
-		if(inputdata[char] == "{" and not inGarbage):
+		if(c == "{" and not inGarbage):
 			groupLevel += 1
-		elif(inputdata[char] == "}" and groupLevel > 0 and not inGarbage):
+		elif(c == "}" and groupLevel > 0 and not inGarbage):
 			score += groupLevel
 			groupLevel -=1
 
-		if(inputdata[char] == "!"):
+		if(c == "!"):
 			char += 2
 		else:
 			char += 1
 
-	print "Score: " + str(score + 1)
+	print "Score: " + str(score + 1) + " Garbage count: " + str(garbageCount)
 
 
 main()
