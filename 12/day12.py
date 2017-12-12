df8cdfda67d5ee7262831a1cb3e17fd62717c09f
Martin Fredriksson <martin.fredriksson@martinfredriksson.com>
2017-12-12 10:24:57 +0100

    Day 12 part 3

--- 12/day12.py
+++ 12/day12.py
@@ -19,11 +19,37 @@ def main():
 	while(len(currentItems) > 0):
 		linkedItems = idToConnection.get(currentItems.pop())
 		for linkedItem in linkedItems:
-			print linkedItem
 			if(not inGroup.has_key(linkedItem)):
 				currentItems.append(linkedItem)
 				inGroup[linkedItem] = 1
 
-	print len(inGroup)
+	print "Task 1: " + str(len(inGroup))
+
+	groups = 0
+	while(findfirst(connections, inGroup) != None):
+		first = findfirst(connections, inGroup)
+		localGroup = dict()
+		currentItems = [str(first)]
+		inGroup[str(first)] = 1
+		localGroup[str(first)] = 1
+
+		while(len(currentItems) > 0):
+			currentItem = currentItems.pop()
+			linkedItems = idToConnection.get(currentItem)
+
+			for linkedItem in linkedItems:
+				if(not localGroup.has_key(linkedItem)):
+					currentItems.append(linkedItem)
+					localGroup[linkedItem] = 1
+					inGroup[linkedItem] = 1
+		groups += 1
+	print "Task 2: " + str(groups+1)
+
+
+def findfirst(connections, inGroup):
+	for x in range(0,len(connections)):
+		if(not inGroup.has_key(str(x))):
+			return x
+	return None
 
 main()
